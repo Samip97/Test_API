83eb94a2454fef6d3cf75f5d92159fe3b0dc9123
unknown <Samip97>
2024-05-31 12:35:45 +0900

    added new login and triggers

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 from typing import Annotated, Union
 import base64 
 import jwt
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Request, File, UploadFile
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
@@ -12,6 +12,10 @@ import ultralytics
 from ultralytics import YOLO
 import cv2
 
+from google.oauth2 import service_account
+from google.cloud import datastore
+from datetime import datetime, timedelta, timezone
+
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -19,39 +23,12 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-fake_users_db = {
-    "rdd": {
-        "username": "rdd",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$qkM0PLWiyqduqdVPM59SHu3K45cf0yW6mS8x8cH5bZYCKxAkH/rS.",
-        "disabled": False,
-    }
-}
+class UserLogin(BaseModel):
+    username:str
+    password:str
 
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: Union[str, None] = None
-
-class ImgData(BaseModel):
-    image_data: str
-
-
-class User(BaseModel):
-    username: str
-    email: Union[str, None] = None
-    full_name: Union[str, None] = None
-    disabled: Union[bool, None] = None
-
-
-class UserInDB(User):
-    hashed_password: str
-
+class ImageName(BaseModel):
+    filename:str
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -68,58 +45,19 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+
+def get_password_with_username(username):
+    credentials = service_account.Credentials.from_service_account_file("secret-node-424009-k6-4dc19cc48467.json")
+    client = datastore.Client(credentials=credentials, database="testbase", namespace="user", project="secret-node-424009-k6")
+    query = client.query(kind="user")
+    query = query.add_filter(filter=datastore.query.PropertyFilter("username", "=", username))
+    results = list(query.fetch())
 
 
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
+    if results:
+        return True, results[0].get("password")
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except InvalidTokenError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+        return False, "user doenot exist"
 
 
 @app.get("/")
@@ -127,49 +65,101 @@ def read_root():
     return "Hello World"
 
 
-@app.get("/testapi")
-def return_api(current_user: Annotated[User, Depends(get_current_active_user)]):
-    return {"Hello": "World"}
+@app.post("/login")
+async def login(userLogin: UserLogin):
+
+    print(userLogin.username.strip(), userLogin.password.strip())
+
+    if userLogin.username.strip() == "" or userLogin.password.strip() == "":
+        return {"success":False, "message": "username or password cannot be empty"}
+
+    userInDBstatus, password = get_password_with_username(userLogin.username)
+    if userInDBstatus == False:
+        return {"success":False, "message": "user doesnot exist"}
+        
+    try:
+        user = verify_password(userLogin.password.strip(), password)
+    except:
+        print("ok")
+        return {"success":False, "message": "incorrect password"}
+                
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    exp_time = datetime.now(timezone.utc) + timedelta(minutes=15)
+   
+
+    payload = {"username": userLogin.username.strip(), "exp": exp_time}
+    access_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    
+    return {"success": True, "token": {"access_token": access_token, "token_type": "bearer"}}
+    
+def verify_token(req: Request):
+    token = req.headers["Authorization"]
+
+    try:
+        jwt.decode(token.split(" ")[-1], SECRET_KEY, algorithms=[ALGORITHM])
+        print(jwt.decode(token.split(" ")[-1], SECRET_KEY, algorithms=[ALGORITHM]))
+        return True
+    except jwt.ExpiredSignatureError:
+        print("has expired")
+        return False
+        
+@app.get("/test")
+async def home(authorized: bool = Depends(verify_token)):
+    if authorized:
+        return {"success": True, "detail": "Welcome home"}
+    else:
+        return {"success": False, "message": "token has expired"}
+
+@app.post("/uploadfile")
+def upload(file: UploadFile = File(...), authorized: bool = Depends(verify_token)):
+    if authorized == False:
+        return {"success": False, "message": "token has expired"}
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
+
+        
 
 @app.post("/inference")
-def inference(img: ImgData, current_user:Annotated[User, Depends(get_current_active_user)]):
+def upload(file: ImageName):
 
-    base64_received = str.encode(img.image_data)
-    image_64_decode = base64.b64decode(base64_received)
-    image_result = open('/tmp/testk.jpg', 'wb') # create a writable image and write the decoding result
-    image_result.write(image_64_decode)
-
-    model = YOLO("model/yolov8n.pt")
-
-
-    results = model("/tmp/testk.jpg")  
-
-    cv2.imwrite("/tmp/pr.jpg", results[0].plot())
-
-
-    image = open('/tmp/pr.jpg', 'rb') #open binary file in read mode
-    image_read = image.read()
-    image_64_encode = base64.b64encode(image_read)
-    return_byte = image_64_encode.decode()
-    
-    return {"success":True, "annotated_image_bytestring": return_byte}
+    print(file.filename)
 
     
 
 
-@app.post("/token")
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-) -> Token:
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return Token(access_token=access_token, token_type="bearer")
+
+# @app.post("/inference")
+# def inference(img: ImgData, current_user:Annotated[User, Depends(get_current_active_user)]):
+
+#     base64_received = str.encode(img.image_data)
+#     image_64_decode = base64.b64decode(base64_received)
+#     image_result = open('/tmp/testk.jpg', 'wb') # create a writable image and write the decoding result
+#     image_result.write(image_64_decode)
+
+#     model = YOLO("model/yolov8n.pt")
+
+
+#     results = model("/tmp/testk.jpg")  
+
+#     cv2.imwrite("/tmp/pr.jpg", results[0].plot())
+
+
+#     image = open('/tmp/pr.jpg', 'rb') #open binary file in read mode
+#     image_read = image.read()
+#     image_64_encode = base64.b64encode(image_read)
+#     return_byte = image_64_encode.decode()
+    
+#     return {"success":True, "annotated_image_bytestring": return_byte}
+
+    
+
